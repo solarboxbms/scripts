7be277cc53a5cd3eb3c5614106d052bae72901e4
Pedro Gracia <pedro.gracia@impulzia.com>
2021-03-31 20:29:43 +0200

    First version to store data from devices

---
+++ battery.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+import base64
+
+CELLS = 13
+ERRORS = 50
+
+res = {}
+
+#_d = 'Qki6AxE7ugMTNboDYvq5AzbeuQPT97kD9em5A8/8uQOT0rkDV/G3AwUOuAPBHLgDZV+7AwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATGwAAAAAAAAAAAAAAAAAAAAAAACc5DAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATvQMAz1wIAFg6AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKq9bTCllKwFtP7//wAAAAAMsokbAAAAABiqsPr/////AAAAAAAAAAAuAAAAZmMAAAC3oQEgD/oAkAGQAQAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAC27ceRAQAAAAAAAAAmXDoWAAAAAAgzfgEAAAAA//8JAFfxtwMMAAAAZV+7AwAAAAAAAAAA'
+d = ''
+
+def decode(_d):
+    global d
+    d = base64.b64decode(_d)
+    # pop
+    def read(q):
+        global d
+        if q <= 0:
+            res = int.from_bytes(d[:4], 'little') / 2**(-q)
+            d = d[4:]
+        else:
+            res = int.from_bytes(d[:int(abs(q/8))], 'little') 
+            d = d[int(abs(q/8)):]
+        return res
+
+    
+    # negatives and 0 are Q numbers
+    # positives are bits
+    cells = {
+        'Voltages': -24,
+        'NumMaxVoltages': 32,
+        'NumMinVoltageses': 32,
+        'Resistances': 32
+    }
+
+    vars = {
+        'Total_Voltage': -24,
+        'Vcapacitor': -24,
+        'Switch_Current': -23,
+        'Switch_RMS_Current': -23,
+        'Cum_Ah_Charge': 64, #-64, # v.5
+        'Cum_Ah_Discharge': 64, #-64 # v.5
+        'Last_Ah_Charge': 32,
+        'Last_Ah_Discharge': 32,
+        'Switch_Temperature': -24,
+        'Micro_Temperature': -24,
+        'Batt_Temperature_1': -24,
+        'Batt_Temperature_2': -24,
+        'SOC': 16,
+        'SOH': 16, 
+        'Status': 8, # bool
+        'Errors (all)': 8,
+        #'Errors_Generic_Error': 8,
+        #'Errors_Current': 8,
+        #'Errors_Voltage': 8,
+        #'Errors_Temperature': 8,
+        #'Errors_Communication': 8,
+        #'Errors_Device_Profile': 8,
+        #'Errors_Reserved': 8,
+        #'Errors_Manufacturer': 8,
+        'Switch_On': 8 # bool
+    }
+
+    vars_internals = {
+        'Times_Main_Interrupt': 32,
+        'First_Update': 16,
+        'Num_Cell_Charging': 16,
+        'Num_Cell_Discharging': 16,
+        'AFE_NumCellMin': 16,
+        'AFE_Vmin': -24,
+        'AFE_NumCellMax': 16,
+        'AFE_Vmax': -25,
+        'Enable_Discharge': 8 # bool
+
+    }
+
+    # cells
+    for kcell, vcell in cells.items():
+        #res[kcell] = {}
+        for i in range(1, CELLS+1):
+            z = read(vcell)
+            #print(f'{kcell}[{i}]', z)
+            res['f{kcell}.{i}'] = z
+
+    # info
+    for kvar, vvar in vars.items():
+        if 'Temp' in kvar:
+            #print(kvar, read(vvar)*10)
+            res[kvar] = read(vvar)*10
+        else:
+            #print(kvar, read(vvar))
+            res[kvar] = read(vvar)
+
+    # errors buffer
+    #res['Errors'] = {}
+    for i in range(1, ERRORS+1):
+        #print(f'Errors[{i}]', read(8))
+        res[f'Errors.{i}'] = read(8)
+
+    #print('NumberOfErrors', read(8))
+    res['NumberOfErrors'] = read(8)
+
+    # timestamp of errors in buffer
+    #res['TimeStamp_Errors'] = {}
+    for i in range(1, ERRORS+1):
+        #print(f'TimeStamp_Errors[{i}]', read(32))
+        res[f'TimeStamp_Errors.{i}'] = read(32)
+
+    #print('TimesCurrentIntegrated', read(32))
+    #print('IsMaster', read(8)) # bool
+    #print('Cum_Ah', read(64))
+    res['TimesCurrentIntegrated'] = read(32)
+    res['IsMaster'] = read(8)
+    res['Cum_Ah'] = read(64)
+
+    # internals info
+    for kvar, vvar in vars_internals.items():
+        #print(kvar, read(vvar))
+        res[kvar] = read(vvar)
+
+    #print(len(d))
+    #print(d)
+    return res
---
+++ sync.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import asyncio
+import time
+import random
+import json
+import battery
+import paho.mqtt.client as mqtt
+from siridb.connector import SiriDBClient
+from asyncio_mqtt import Client, MqttError
+from periodic import Periodic
+from config import SIRIDB, MQTT
+
+UUIDS = [
+    '16ed78a6-4357-4742-a759-d605bb2ef75b',
+    '3ca4664e-212e-4da2-ab7b-666e35e3ccd0',
+    '455dbde7-18fe-4e49-a59a-acb0acdaaa04',
+    '4db569d3-19b4-4e9f-9ef7-fef0d6832976',
+    '5f884f63-0305-42b3-8c7c-9fddf08f490a',
+    'b5c1920e-a900-4a9f-ad06-f476768f4528',
+    'd3477f15-f827-4c97-9a39-f93c4e505b01',
+    'd4db3a5c-669a-4b8f-afa4-df92298bffe5',
+    'e06b98d8-205b-4fff-845f-f24d6a391856',
+    'f42b6726-4914-4511-ad25-5192f330ed4e'
+]
+
+siri = SiriDBClient(
+    username=SIRIDB.username,
+    password=SIRIDB.password,
+    dbname=SIRIDB.dbname,
+    hostlist=[(SIRIDB.host, SIRIDB.port)],  # Multiple connections are supported
+    keepalive=True)
+
+
+async def update(siri):
+    # Start connecting to SiriDB.
+    # .connect() returns a list of all connections referring to the supplied
+    # hostlist. The list can contain exceptions in case a connection could not
+    # be made.
+
+    p = Periodic(MQTT.period, action_battery)
+    await p.start()
+
+    await siri.connect()
+
+    async with Client(MQTT.host) as client:
+        async with client.filtered_messages(f'{MQTT.domain}/+/out') as messages:
+            await client.subscribe(f'{MQTT.domain}/#')
+            async for message in messages:
+                try:
+                    m = json.loads(message.payload.decode())
+                except:
+                    m = []
+                uuid = message.topic.split('/')[1]
+                ts = int(time.time())
+                if 'uptime' in m:
+                    #print(uuid, m['uptime'])
+                    await siri.insert({f'{uuid}.uptime': [[ts, m['uptime']]]})
+                elif 'data' in m:
+                    _data = battery.decode(m['data'])
+                    #print(uuid, res)
+                    print(f'Sending data for {uuid}...')
+                    data = {}
+                    for kdata, vdata in _data.items():
+                        if kdata.lower() in ['cum_ah_discharge']:
+                            continue
+                        data[f'{uuid}.{kdata.lower()}'] = [[ts, vdata]]
+
+                    print(data)
+                    await siri.insert(data)
+    siri.close()
+
+
+async def action_battery():
+    async with Client(MQTT.host) as client:
+        for uuid in UUIDS:
+            message = 'battery'
+            topic = f'{MQTT.domain}/{uuid}/action'
+            print(f'[topic="{topic}"] Publishing message={message}')
+            await client.publish(topic, message, qos=1)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(update(siri))
