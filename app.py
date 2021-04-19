dae30a779e797dffec75c47482c9373c31765540
Pedro Gracia <pedro@impulso.xyz>
2021-04-19 18:52:47 +0200

    Changes in UUIDs

--- app.py
+++ app.py
@@ -12,29 +12,17 @@ from datetime import datetime
 from siridb.connector import SiriDBClient
 #from asyncio_mqtt import Client, MqttError
 from periodic import Periodic
-from config import SIRIDB #, MQTT
+from config import SIRIDB, GROUPS, DEVICES, UUIDS #, MQTT
 from flask import Flask, escape, request, render_template, redirect, url_for
 
-UUIDS = [
-    '16ed78a6-4357-4742-a759-d605bb2ef75b',
-    '3ca4664e-212e-4da2-ab7b-666e35e3ccd0',
-    '455dbde7-18fe-4e49-a59a-acb0acdaaa04',
-    '4db569d3-19b4-4e9f-9ef7-fef0d6832976',
-    '5f884f63-0305-42b3-8c7c-9fddf08f490a',
-    'b5c1920e-a900-4a9f-ad06-f476768f4528',
-    'd3477f15-f827-4c97-9a39-f93c4e505b01',
-    'd4db3a5c-669a-4b8f-afa4-df92298bffe5',
-    'e06b98d8-205b-4fff-845f-f24d6a391856',
-    'f42b6726-4914-4511-ad25-5192f330ed4e'
-]
 
-
-async def show(siri, id):
+async def show_device(siri, id):
     # TODO: use pool
     data = {}
     await siri.connect()
     res = await siri.query(f'select last() from /{id}.*/ after now - 1h')
     siri.close()
+    print(res)
     if f'{id}.voltages.1' in res:
         delta = datetime.now() - datetime.fromtimestamp(res[f'{id}.voltages.1'][0][0])
         moment = humanize.naturaldelta(delta)
@@ -45,18 +33,24 @@ async def show(siri, id):
             data[k[37:]] = v
     return moment, data
 
-async def online(siri):
-    data = {}
+async def show_devices(siri):
+    data = dict([i, {}] for i in GROUPS)
     await siri.connect()
-    res = await siri.query(f'select last() from /.*.uptime/ after now - 1h')
+    # uptime
+    res_uptime = await siri.query(f'select last() from /.*.uptime/ after now - 1h')
+    res_voltage = await siri.query(f'select last() from /.*.total_voltage/ after now - 1h')
     siri.close()
 
     for k,v in res.items():
         if v:
             delta = datetime.now() - datetime.fromtimestamp(v[0][0])
             moment = humanize.naturaldelta(delta)
-            data[k.split('.')[0]] = moment
-    
+            key = k.split('.')[0]
+            group = UUIDS[key]
+            data[group][key] = {
+                'name': DEVICES[group][key]['name'],
+                'since': moment
+            }
     return data
 
 
@@ -78,12 +72,12 @@ def device(id=None):
     #tasks = [asyncio.async(show(siri,id))]
     #tasks = [getattr(asyncio, 'async')(show(siri,id))]
     #loop = asyncio.get_event_loop()
-    moment, data  = loop.run_until_complete(show(siri,id))
+    moment, data  = loop.run_until_complete(show_device(siri,id))
     return render_template('device.html', id=id, moment=moment, data=dict(reversed(list(data.items()))))
 
 @app.route('/devices')
 def devices():
-    data  = loop.run_until_complete(online(siri))
+    data  = loop.run_until_complete(show_devices(siri))
     return render_template('devices.html', data=data)
 
 @app.route('/')
--- sync.py
+++ sync.py
@@ -9,20 +9,7 @@ import paho.mqtt.client as mqtt
 from siridb.connector import SiriDBClient
 from asyncio_mqtt import Client, MqttError
 from periodic import Periodic
-from config import SIRIDB, MQTT
-
-UUIDS = [
-    '16ed78a6-4357-4742-a759-d605bb2ef75b',
-    '3ca4664e-212e-4da2-ab7b-666e35e3ccd0',
-    '455dbde7-18fe-4e49-a59a-acb0acdaaa04',
-    '4db569d3-19b4-4e9f-9ef7-fef0d6832976',
-    '5f884f63-0305-42b3-8c7c-9fddf08f490a',
-    'b5c1920e-a900-4a9f-ad06-f476768f4528',
-    'd3477f15-f827-4c97-9a39-f93c4e505b01',
-    'd4db3a5c-669a-4b8f-afa4-df92298bffe5',
-    'e06b98d8-205b-4fff-845f-f24d6a391856',
-    'f42b6726-4914-4511-ad25-5192f330ed4e'
-]
+from config import SIRIDB, MQTT, DEVICES, UUIDS
 
 siri = SiriDBClient(
     username=SIRIDB.username,
@@ -73,7 +60,7 @@ async def update(siri):
 
 async def action_battery():
     async with Client(MQTT.host) as client:
-        for uuid in UUIDS:
+        for uuid in UUIDS.keys():
             message = 'battery'
             topic = f'{MQTT.domain}/{uuid}/action'
             print(f'[topic="{topic}"] Publishing message={message}')
